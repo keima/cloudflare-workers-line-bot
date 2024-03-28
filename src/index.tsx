4483e9e8057917a6565e091ff474b4c40ccba258
Kouta Imanaka <keima@p-side.net>
2024-03-28 21:16:37 +0900

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,15 @@
 import { Hono } from 'hono'
-import type { FC } from 'hono/jsx'
-import { webhook, messagingApi } from '@line/bot-sdk'
+import { showRoutes } from 'hono/dev'
+import { Env } from './types';
 
-const Layout: FC = ({ children }) => (
-  <html>
-    <body>{children}</body>
-  </html>
-);
+import lineWebhook from './lineWebhook';
 
-type Bindings = {
-    LINE_CHANNEL_ACCESS_TOKEN: string
-}
+const app = new Hono<Env>()
 
-const app = new Hono<{ Bindings: Bindings }>()
+app.get('/', (c) => c.text("cloudflare-workers-line-bot"))
 
-app.get('/', (c) => c.text("<Layout>Hello World</Layout>"))
+app.route('/webhook', lineWebhook)
 
-app.post('/webhook', async (c) => {
-  const payload = await c.req.json<webhook.CallbackRequest>()
-
-  console.log("/webhook")
-  console.log(`${JSON.stringify(payload)}`)
-
-  const api = new messagingApi.MessagingApiClient({
-      channelAccessToken: c.env.LINE_CHANNEL_ACCESS_TOKEN,
-  })
-
-  for(const event of payload.events) {
-      if(event.type === 'message') {
-          const messageEvent = (event as webhook.MessageEvent)
-          const message = messageEvent.message
-          if(message.type === 'text') {
-              if (messageEvent.replyToken != null) {
-                  const text = (message as webhook.TextMessageContent).text
-                  await api.replyMessage({
-                      replyToken: messageEvent.replyToken,
-                          messages: [
-                              {
-                                  type: 'text',
-                                  text: `🦜 < ${text}`,
-                              },
-                          ]
-                  })
-                  // TODO: error handling
-              }
-          }
-      }
-  }
-
-  return c.json({
-      message: 'ok'
-  })
-})
+showRoutes(app)
 
 export default app
