de1dce7aab929161084fc652b815f9254862f14b
Kouta Imanaka <keima@p-side.net>
2024-03-28 21:14:14 +0900

    rewrite to using line-bot-sdk

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { Hono } from 'hono'
 import type { FC } from 'hono/jsx'
-import { Client, webhook, messagingApi } from '@line/bot-sdk'
+import { webhook, messagingApi } from '@line/bot-sdk'
 
 const Layout: FC = ({ children }) => (
-    <html>
-        <body>{children}</body>
-    </html>
+  <html>
+    <body>{children}</body>
+  </html>
 );
 
 type Bindings = {
@@ -17,82 +17,40 @@ const app = new Hono<{ Bindings: Bindings }>()
 app.get('/', (c) => c.text("<Layout>Hello World</Layout>"))
 
 app.post('/webhook', async (c) => {
-    const payload = await c.req.json<webhook.CallbackRequest>()
+  const payload = await c.req.json<webhook.CallbackRequest>()
 
-    console.log("/webhook")
-    console.log(`${JSON.stringify(payload)}`)
+  console.log("/webhook")
+  console.log(`${JSON.stringify(payload)}`)
 
-    // const client = new Client({
-    //     channelAccessToken: c.env.LINE_CHANNEL_ACCESS_TOKEN,
-    //     httpConfig: {
-    //         adapter: fetchAdapter,
-    //     }
-    // })
+  const api = new messagingApi.MessagingApiClient({
+      channelAccessToken: c.env.LINE_CHANNEL_ACCESS_TOKEN,
+  })
 
-    // const api = new messagingApi.MessagingApiClient({
-    //     channelAccessToken: c.env.LINE_CHANNEL_ACCESS_TOKEN,
-    // })
+  for(const event of payload.events) {
+      if(event.type === 'message') {
+          const messageEvent = (event as webhook.MessageEvent)
+          const message = messageEvent.message
+          if(message.type === 'text') {
+              if (messageEvent.replyToken != null) {
+                  const text = (message as webhook.TextMessageContent).text
+                  await api.replyMessage({
+                      replyToken: messageEvent.replyToken,
+                          messages: [
+                              {
+                                  type: 'text',
+                                  text: `🦜 < ${text}`,
+                              },
+                          ]
+                  })
+                  // TODO: error handling
+              }
+          }
+      }
+  }
 
-    for(const event of payload.events) {
-        if(event.type === 'message') {
-            const messageEvent = (event as webhook.MessageEvent)
-            const message = messageEvent.message
-            if(message.type === 'text') {
-                if (messageEvent.replyToken != null) {
-                    const text = (message as webhook.TextMessageContent).text
-
-/*
-curl -v -X POST https://api.line.me/v2/bot/message/reply \
--H 'Content-Type: application/json' \
--H 'Authorization: Bearer {channel access token}' \
--d '{
-    "replyToken":"*****",
-    "messages":[
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-    ]
-}'
-*/
-                    await fetch("https://api.line.me/v2/bot/message/reply", {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${c.env.LINE_CHANNEL_ACCESS_TOKEN}`
-                        },
-                        body: JSON.stringify({
-                            replyToken: messageEvent.replyToken,
-                            messages: [
-                                {
-                                    type: 'text',
-                                    text: `🦜 < ${text}`,
-                                },
-                            ]
-                        }),
-                    })
-
-                    // await client.replyMessage(
-                    //     messageEvent.replyToken,
-                    //     [
-                    //         {
-                    //             type: 'text',
-                    //             text: `🦜 < ${text}`,
-                    //         },
-                    //     ]
-                    // )
-                }
-            }
-        }
-    }
-
-    return c.json({
-        message: 'ok'
-    })
+  return c.json({
+      message: 'ok'
+  })
 })
 
 export default app
